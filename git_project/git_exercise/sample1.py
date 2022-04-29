7352012516127f4f9581685eff15813417776a47
saidakhonsadikova <saidausa.sadikova@gmail.com>
2022-04-28 22:04:17 -0400

    upodate scenario2

---
+++ git_project/git_exercise/sample1.py
@@ -0,0 +1,14 @@
+#sample file for git
+print("hello world!")
+print("more lines in the sample1")
+for i in range(10):
+  print(f"numbers:{1}")
+
+
+  #hw Register to the github and save your email password
+print("New changes after last push, fixed proj-23455")
+
+
+
+
+
