cc9f74a9ecddb638295c939b9393e35b37ff8760
saidakhonsadikova <saidausa.sadikova@gmail.com>
2022-05-03 20:12:09 -0400

    new line added, jira#

---
+++ git_project/git_exercise/sample1.py
@@ -0,0 +1,9 @@
+#sample file for git
+print("hello world")
+print("moire lines in the sample1")
+for i in range(10):
+    print(f"numbers:{1}")
+
+
+#:Register to the github and save your email, password
+print("New changesbafter last push, fixed PROJ-23455")
